431228dba626feae399c7e7e05511e5d1c7d188f
Deyan Kamburov <dkamburov@infragistics.com>
2019-08-05 18:44:10 +0300

    Remove unnecesesery pages and add basic description of the API service parameters

--- IgniteUIThemeService/Controllers/HomeController.cs
+++ IgniteUIThemeService/Controllers/HomeController.cs
@@ -26,20 +26,6 @@ namespace IgniteUIThemeService.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
